5dd5648640ac2eb9cf79334c023891b60b421ddf
JDTobin <90791836+JDTobin@users.noreply.github.com>
2022-05-10 17:21:45 -0400

    replace spaces with underscores in names

--- app/core/models.py
+++ app/core/models.py
@@ -34,6 +34,7 @@ class TermSet(TimeStampedModel):
 
     def save(self, *args, **kwargs):
         """Generate iri for item"""
+        self.name = self.name.replace(' ', '_')
         self.iri = 'xss:' + self.version + '@' + self.name
         update_fields = kwargs.get('update_fields', None)
         if update_fields:
@@ -49,6 +50,7 @@ class ChildTermSet(TermSet):
 
     def save(self, *args, **kwargs):
         """Generate iri for item"""
+        self.name = self.name.replace(' ', '_')
         self.iri = self.parent_term_set.iri + '/' + self.name
         self.version = self.parent_term_set.version
         update_fields = kwargs.get('update_fields', None)
@@ -90,6 +92,7 @@ class Term(TimeStampedModel):
 
     def save(self, *args, **kwargs):
         """Generate iri for item"""
+        self.name = self.name.replace(' ', '_')
         self.iri = self.term_set.iri + '?' + self.name
         update_fields = kwargs.get('update_fields', None)
         if update_fields:
